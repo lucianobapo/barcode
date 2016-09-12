1c9e72cec53941cb71527538f4648137d61c63e1
Luciano Porto <luciano.bapo@gmail.com>
2016-09-12 17:28:52 -0300

    first commit

--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class CodeController extends Controller
 {
+    use ValidatesRequests;
+
     public function show()
     {
         return view('code');
@@ -15,10 +18,41 @@ class CodeController extends Controller
 
     public function code(Request $request)
     {
-        $requestData = $request->all();
-        return view('code')->with([
-            'code' => $requestData['code'],
-            'codeImage' => \DNS1D::getBarcodePNG($requestData['code'], "EAN13",1),
+        $this->validate($request, [
+            'code' => 'required|numeric',
         ]);
+
+        $requestData = $request->all();
+
+        $ean13Digit = $this->ean13Digit($requestData['code']);
+
+        return view('code')->with([
+            'code' => $ean13Digit,
+            'codeImage' => \DNS1D::getBarcodePNG($ean13Digit, "EAN13", 1),
+        ]);
+    }
+
+    /**
+     * @param $code
+     * @return string
+     */
+    private function ean13Digit($code)
+    {
+        $somaImpar = 0;
+        $somaPar = 0;
+
+        for ($i = 0; $i < strlen($code); $i++) {
+
+            if (($i % 2) == 1) //é ímpar
+                $somaImpar = $somaImpar + $code[$i];
+            if (($i % 2) == 0) //é par
+                $somaPar = $somaPar + $code[$i];
+        }
+        $somaResultados = $somaPar + ($somaImpar * 3);
+
+        if (($somaResultados % 10)==0)
+            return $code.'0';
+        else
+            return $code.(10-($somaResultados % 10));
     }
 }
